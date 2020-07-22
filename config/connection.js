f3451b6fcdbd9dde79cd3638d667dc54cb1e9f6a
AKKramer <austinkkramer@gmail.com>
2020-07-21 21:12:17 -0500

    Updated with JAWS_DB connection option

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
 const mysql = require('mysql');
 const util = require('util');
 require('dotenv').config()
+var connection;
 
-
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: 'noteTaker_db'
   });
+}
+
 
 connection.connect(function(err) {
   if (err) {
